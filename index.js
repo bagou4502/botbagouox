23cc9d87823e45f93f216f900177cea499a5fffc
bagou4502 <bagou450roro@gmail.com>
2021-12-24 10:49:50 +0100

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const config = require('./config/config.json');
+
+const intents = new Discord.intents('32767');
+
+client.once('ready', () => {
+    console.log('Bagouox is online!');
+});
+
+client.login(config.token);
